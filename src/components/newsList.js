2b4c1c66513a1154c93b0472a5b64ff2adf10d16
Le Trong Dat <datlt2306@gmail.com>
2022-01-21 14:46:32 +0700

    refactor: using axios for  detail and post list

--- src/components/newsList.js
+++ src/components/newsList.js
@@ -1,10 +1,12 @@
 // import data from "../data";
 
+import axios from 'axios';
+
 const NewsList = {
-    render() {
-        return fetch("https://5e79b4b817314d00161333da.mockapi.io/posts")
-            .then((response) => response.json())
-            .then((data) => /* html */`
+    async render() {
+        const { data } = await axios.get('https://5e79b4b817314d00161333da.mockapi.io/posts');
+        console.log(data);
+        return /* html */`
                 <h2 class="font-semibold text-2xl my-4 text-blue-900 uppercase">Tin tức học tập</h2>
                 <div class="grid grid-cols-3 gap-8">
                     ${data.map((post) => /* html */`
@@ -17,7 +19,24 @@ const NewsList = {
                         </div>
                     `).join("")}
                 </div>
-            `);
+            `
+
+            // return fetch("https://5e79b4b817314d00161333da.mockapi.io/posts")
+            // .then((response) => response.json())
+            // .then((data) => /* html */`
+            //     <h2 class="font-semibold text-2xl my-4 text-blue-900 uppercase">Tin tức học tập</h2>
+            //     <div class="grid grid-cols-3 gap-8">
+            //         ${data.map((post) => /* html */`
+            //                 <div class="border p-4">
+            //                 <a href="/news/${post.id}">
+            //                     <img src="${post.img}" alt="" />                    
+            //                 </a>
+            //                 <h3 class="my-3"><a href="#" class="font-semibold text-orange-500 text-lg">${post.title}</a></h3>
+            //                 <p class="text-sm text-gray-500">${post.desc} </p>
+            //             </div>
+            //         `).join("")}
+            //     </div>
+            // `);
     },
 };
 export default NewsList;
