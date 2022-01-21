3eca0a5ce11435ebe1ffe99ea59bbf7dde3c0c62
Le Trong Dat <datlt2306@gmail.com>
2022-01-21 16:36:13 +0700

    refactor: using axios

--- src/components/news.js
+++ src/components/news.js
@@ -1,27 +1,50 @@
-import data from "../data";
 import style from "./news.module.css";
-
+import axios from 'axios';
 const News = {
-    render() {
-        return fetch("https://5e79b4b817314d00161333da.mockapi.io/posts")
-            .then((response) => response.json())
-            .then((data) => /* html */`
-                    <h2 class="font-semibold text-2xl text-blue-900 my-4 uppercase">Tin tức học lập</h2>
-                    <div class=${style.newsDemo}>
-                        <div class="grid grid-cols-3 gap-8">
-                                ${data.map((post) => `
-                                            <div class="${style["news-item"]} border p-4">
-                                                <div class="news-img">
-                                                    <a href="/news/${post.id}">
-                                                        <img src="${post.img}" />
-                                                    </a>
-                                                </div>
-                                                <h3 class="my-3"><a href="/news/${post.id}" class="font-semibold text-orange-500">${post.title}</a></h3>
-                                                <p class="text-sm text-gray-600">${post.desc}</p>
-                                            </div>
-                                    `).join("")}
-                        </div>
-                    </div>`);
+    async render() {
+        const { data } = await axios.get('https://5e79b4b817314d00161333da.mockapi.io/posts');
+        return /* html */`
+            <h2 class="font-semibold text-2xl text-blue-900 my-4 uppercase">Tin tức học lập</h2>
+            <div>
+                <div class="grid grid-cols-3 gap-8">
+                        ${data.map((post) => `
+                                    <div class="${style["news-item"]} border p-4">
+                                        <div class="news-img">
+                                            <a href="/news/${post.id}">
+                                                <img src="${post.img}" />
+                                            </a>
+                                        </div>
+                                        <h3 class="my-3"><a href="/news/${post.id}" class="font-semibold text-orange-500">${post.title}</a></h3>
+                                        <p class="text-sm text-gray-600">${post.desc}</p>
+                                    </div>
+                            `).join("")}
+                </div>
+            </div>`;
+
+
+        
+    // return fetch("https://5e79b4b817314d00161333da.mockapi.io/posts")
+    // .then((response) => response.json())
+    // .then((data) => /* html */`
+    //         <h2 class="font-semibold text-2xl text-blue-900 my-4 uppercase">Tin tức học lập</h2>
+    //         <div class=${style.newsDemo}>
+    //             <div class="grid grid-cols-3 gap-8">
+    //                     ${data.map((post) => `
+    //                                 <div class="${style["news-item"]} border p-4">
+    //                                     <div class="news-img">
+    //                                         <a href="/news/${post.id}">
+    //                                             <img src="${post.img}" />
+    //                                         </a>
+    //                                     </div>
+    //                                     <h3 class="my-3"><a href="/news/${post.id}" class="font-semibold text-orange-500">${post.title}</a></h3>
+    //                                     <p class="text-sm text-gray-600">${post.desc}</p>
+    //                                 </div>
+    //                         `).join("")}
+    //             </div>
+    //         </div>`);
     },
 };
 export default News;
+
+
+
--- src/pages/detailNews.js
+++ src/pages/detailNews.js
@@ -1,14 +1,15 @@
 // import data from "../data";
 
+import axios from "axios";
+
 const DetailNewsPage = {
-    render(id) {
-        return fetch(`https://5e79b4b817314d00161333da.mockapi.io/posts/${id}`)
-            .then((response) => response.json())
-            .then((data) => `
+    async render(id) {
+        const { data } = await axios.get(`https://5e79b4b817314d00161333da.mockapi.io/posts/${id}`);
+        return `
             <h1>${data.title}</h1>
             <img src="${data.img}" />
             <div>${data.desc}</div>
-        `);
+        `
     },
 };
 export default DetailNewsPage;
