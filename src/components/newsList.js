be20373ae0a7157f3de7ed2e56aaa000eca08cd1
Le Trong Dat <datlt2306@gmail.com>
2022-01-21 12:40:35 +0700

    refactor: add axios library

--- src/components/newsList.js
+++ src/components/newsList.js
@@ -1,24 +1,25 @@
 // import data from "../data";
+import axios from 'axios';
 
 const NewsList = {
-    render() {
-        return fetch("https://5e79b4b817314d00161333da.mockapi.io/posts")
-            .then((response) => response.json())
-            .then((data) =>/* html */`
-                <h2 class="font-semibold text-2xl uppercase text-blue-800 my-4">Tin tức học tập</h2>
-                <div class="grid grid-cols-3 gap-8">
-                    ${data.map((post) => `
-                        <div class="border p-4">
-                            <a href="/news/${post.id}">
-                                <img src="${post.img}" alt="" />
-                            </a>
-                            <h3 class="my-3"><a  href="/news/${post.id}" class="font-semibold text-lg text-orange-500">${post.title}</a></h3>                    
-                            <p>${post.desc}</p>
-                        </div>
-                    `).join("")}
-                </div>
-        
-        `);
+    async render() {
+        // Chờ thằng axios.get truy cập API và lấy dữ liệu, 
+        // lấy dữ liệu xong sẽ trả về và gán vào biến response
+        const { data } = await axios.get("https://5e79b4b817314d00161333da.mockapi.io/posts");
+        return /* html */`
+            <h2 class="font-semibold text-2xl uppercase text-blue-800 my-4">Tin tức học tập</h2>
+            <div class="grid grid-cols-3 gap-8">
+                ${data.map((post) => `
+                    <div class="border p-4">
+                        <a href="/news/${post.id}">
+                            <img src="${post.img}" alt="" />
+                        </a>
+                        <h3 class="my-3"><a  href="/news/${post.id}" class="font-semibold text-lg text-orange-500">${post.title}</a></h3>                    
+                        <p>${post.desc}</p>
+                    </div>
+                `).join("")}
+            </div>
+        `
     },
 };
 export default NewsList;
