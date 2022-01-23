6381b19e257ad81429ca69c11d8234e32219f08c
Le Trong Dat <datlt2306@gmail.com>
2022-01-23 09:36:16 +0700

    update

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://5e79b4b817314d00161333da.mockapi.io'
+    baseURL: 'http://localhost:3001'
 });
 export default instance;
