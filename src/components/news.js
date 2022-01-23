42c1600fcaa00b02854ea5cc6308dc24a371175b
Le Trong Dat <datlt2306@gmail.com>
2022-01-23 09:44:13 +0700

    refactor: using axios

--- src/components/news.js
+++ src/components/news.js
@@ -1,11 +1,9 @@
-import news from "../data";
 
+import axios from 'axios';
 const News = {
-    render() {
-        return fetch('http://localhost:3001/posts')
-            .then(response => response.json())
-            .then(data => {
-                return /* html */`
+    async render() {
+        const { data } = await axios.get('http://localhost:3001/posts');
+        return /* html */`
                     <h2 class="font-semibold text-blue-900 my-4 uppercase text-2xl">Tin tức học tập</h2>
                     <div class="grid grid-cols-3 gap-8">
                         ${data.map((post) => `
@@ -19,7 +17,25 @@ const News = {
                     </div>
                     
                     `;
-            })
+
+        // return fetch('http://localhost:3001/posts')
+        //     .then(response => response.json())
+        //     .then(data => {
+        //         return /* html */`
+        //             <h2 class="font-semibold text-blue-900 my-4 uppercase text-2xl">Tin tức học tập</h2>
+        //             <div class="grid grid-cols-3 gap-8">
+        //                 ${data.map((post) => `
+        //                 <div class="border p-3">
+        //                     <a href="/product/${post.id}"><img src="${post.img}" alt="" /></a>
+        //                     <h3 class="my-3"><a href="/product/${post.id}" class="font-semibold text-orange-500 text-lg">${post.title}</a></h3>
+        //                     <p>${post.desc}</p>
+        //                 </div>
+        //                 `).join("")}
+                        
+        //             </div>
+                    
+        //             `;
+        //     })
         
     },
 };
