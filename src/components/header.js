0d32c723950c1e27f49bc9b171229d6456a72b2e
Le Trong Dat <datlt2306@gmail.com>
2022-01-23 10:59:28 +0700

    style: :fire: Add module toast

--- src/components/header.js
+++ src/components/header.js
@@ -4,8 +4,9 @@ const Header = {
         <div class="bg-orange-500">
             <ul class="flex">
             <li><a href="/" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white">Home Page</a></li>
-            <li><a href="/about" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white">About Page</a></li>
-            <li><a href="/product" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white">Product Page</a></li>
+            <li><a href="/#/about" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white">About Page</a></li>
+            <li><a href="/#/product" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white">Product Page</a></li>
+            <li><a href="/#/admin/dashboard" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white">Dashboard Page</a></li>
             </ul>
         </div>
         `;
