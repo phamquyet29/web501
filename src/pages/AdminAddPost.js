729ab1d18f7f79d7204df848299640928030278a
Le Trong Dat <datlt2306@gmail.com>
2022-02-08 12:30:41 +0700

    feat: upload image

--- src/pages/AdminAddPost.js
+++ src/pages/AdminAddPost.js
@@ -11,20 +11,21 @@ const AddPost = {
             <div>
             <form action="" id="form-add-post">
                 <input type="text"
-                id="title-post"
-                class="border border-black"
-                placeholder="Title post"
-                > <br />
-                <input type="text"
-                id="img-post"
-                class="border border-black"
-                placeholder="Imager post"
+                    id="title-post"
+                    class="border border-black"
+                    placeholder="Title post"
+                > 
+                <br />
+                <input type="file"
+                    id="img-post"
+                    class="border border-black"
+                    placeholder="Imager post"
                 > <br />
                 <textarea name="" 
-                id="desc-post" 
-                class="border border-black"
-                cols="30" 
-                rows="10"></textarea>
+                    id="desc-post" 
+                    class="border border-black"
+                    cols="30" 
+                    rows="10"></textarea>
                 <button class="bg-blue-500 p-4 inline-block text-white">Thêm bài viết</button>
             </form>
             </div>
@@ -32,16 +33,37 @@ const AddPost = {
     },
     afterRender(){
         const formAdd = document.querySelector('#form-add-post');
+        const imgPost = document.querySelector('#img-post');
+
+        const CLOUDINARY_API = "https://api.cloudinary.com/v1_1/ecommercer2021/image/upload";
+        const CLOUDINARY_PRESET = "jkbdphzy";
+
         formAdd.addEventListener('submit', (e) => {
             e.preventDefault();
-            const dataFake = {
-                "title": "Bai viet moi vua them 01",
-                "img": "https://picsum.photos/200/200",
-                "desc": "Mo ta san pham"
-            }
 
-            // call api thêm sản phẩm
-            axios.post('http://localhost:3001/posts', dataFake)
+            const file = imgPost.files[0];
+
+            const formData = new FormData();
+            formData.append('file', file);
+            formData.append('upload_preset', CLOUDINARY_PRESET)
+            
+            
+            // call api cloudinary
+            axios.post(CLOUDINARY_API,formData, {
+                headers: {
+                    "Content-Type": "application/form-data"
+                }
+            })
+
+
+            // const dataFake = {
+            //     "title": "Bai viet moi vua them 01",
+            //     "img": "https://picsum.photos/200/200",
+            //     "desc": "Mo ta san pham"
+            // }
+
+            // // call api thêm sản phẩm
+            // axios.post('http://localhost:3001/posts', dataFake)
         })
     }
 };
