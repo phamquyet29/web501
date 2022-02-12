4cb2a28567df6d5600bd5f48e8c555d04863d125
Le Trong Dat <datlt2306@gmail.com>
2022-02-12 14:39:34 +0700

    feat: notify after signup

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,5 +1,7 @@
 import { signup } from "../api/user";
 import { $ } from "../utils/selector";
+import toastr from "toastr";
+import "toastr/build/toastr.min.css";
 
 const Signup = {
     render(){
@@ -12,12 +14,26 @@ const Signup = {
         `
     },
     afterRender(){
-        $('#formSignup').addEventListener('submit', function(e){
+        $('#formSignup').addEventListener('submit', async function(e){
             e.preventDefault();
-            signup({
-                email: $('#email').value,
-                password: $('#password').value,
-            })
+            try {
+                const { data } = await signup({
+                    email: $('#email').value,
+                    password: $('#password').value,
+                });
+                toastr.success("Đăng ký thành công")
+                if(data){
+                    setTimeout(function(){
+                        document.location.href="/signin"
+                    },2000);
+                }
+                
+            } catch (error) {
+                toastr.error(error.response.data)
+                $('#formSignup').reset()
+            }
+           
+
         })
     }
 }
