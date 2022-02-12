2b33c9a6e6045e5bccefa5626051e69367fcb29f
Le Trong Dat <datlt2306@gmail.com>
2022-02-12 17:41:44 +0700

    refactor: logout

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,5 @@
+import { reRender } from "../utils";
+
 const Header = {
     render() {
         return /* html */`<nav class="max-w-5xl mx-auto bg-orange-500 flex items-center justify-between">
@@ -6,18 +8,28 @@ const Header = {
                 <li><a href="/about" class="block px-4 py-3 hover:bg-blue-800 hover:text-white">About Page</a></li>
                 <li><a href="/product" class="block px-4 py-3 hover:bg-blue-800 hover:text-white">Product Page</a></li>
             </ul>
-            <ul class="flex">
+            ${localStorage.getItem('user') ? `
+              <ul class="flex">
                 <li class="flex items-center">Xin chao <a href="/" class="block px-4 py-3 hover:bg-blue-800 hover:text-white" id="email"></a></li>
+                <li class="flex items-center"><a class="block px-4 py-3 hover:bg-blue-800 hover:text-white" id="logout">Logout</a></li>
             </ul>
+            `: ""}
+            
         </nav>`;
     },
     afterRender(){
       // Lấy thông tin từ localStorage
       // Sử dụng JSON.parse để chuyển đổi chuỗi sang object
-      console.log();
       const email = document.querySelector('#email');
+      const logout =  document.querySelector('#logout');
       if(email){
         email.innerHTML = JSON.parse(localStorage.getItem('user')).email;
+      }
+      if(logout){
+        logout.addEventListener('click', function(){
+          localStorage.removeItem('user');
+          reRender(Header, "#header");
+        })
       }
     }
 };
