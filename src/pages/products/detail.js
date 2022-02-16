a9f22813e823bcb4497fa279f3c82e85b0f934e6
Le Trong Dat <datlt2306@gmail.com>
2022-02-16 14:44:08 +0700

    feat: add to cart

--- src/pages/products/detail.js
+++ src/pages/products/detail.js
@@ -1,4 +1,8 @@
 import { get } from '../../api/product';
+import { addToCart } from '../../utils/cart';
+import toastr from 'toastr';
+import "toastr/build/toastr.min.css";
+
 
 const DetailProductPage = {
     async render(id) {
@@ -9,9 +13,21 @@ const DetailProductPage = {
                 <img src="${data.img}" />
                 <p>${data.desc}</p>
                 <p>${data.price}</p>
-                <button class="inline-block px-4 py-3 text-white bg-indigo-500 text-lg">Thêm vào giỏ hàng</button>
+                <input type="number" id="inputQuantity" value="1" class="border border-black" />
+                <button id="btnAddToCart" class="inline-block px-4 py-3 text-white bg-indigo-500 text-lg">Thêm vào giỏ hàng</button>
             </div>
         `
     },
+    afterRender(id){
+        const btnAddToCart = document.querySelector('#btnAddToCart');
+        const inputQuantity = document.querySelector('#inputQuantity');
+
+        btnAddToCart.addEventListener('click', async () => {
+            const { data } = await get(id);
+            addToCart({...data, quantity: +inputQuantity.value}, function(){
+                toastr.success("Thêm thành công");
+            });
+        })
+    }
 };
 export default DetailProductPage;
