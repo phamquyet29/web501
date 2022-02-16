dfc785667070797348ab2a45a904a9d732cdb4ee
Le Trong Dat <datlt2306@gmail.com>
2022-02-16 17:45:46 +0700

    feat: Add to cart

--- src/pages/product/detail.js
+++ src/pages/product/detail.js
@@ -1,4 +1,6 @@
 import { get } from "../../api/product";
+import { addToCart } from "../../utils/cart";
+import { $ } from "../../utils/selector";
 
 const DetailProductPage = {
     async render(id) {
@@ -7,8 +9,15 @@ const DetailProductPage = {
             <h1>${data.name}</h1>
             <img src="${data.img}" />
             <div>${data.desc}</div>
-            <button class="inline-block px-4 py-3 text-white bg-indigo-500 rounded hover:bg-indigo-700">Add to cart</button>
+            <input type="number" id="inputQty" class="border border-gray-400 p-3" value="1" />
+            <button id="btnAddToCart" class="inline-block px-4 py-3 text-white bg-indigo-500 rounded hover:bg-indigo-700">Add to cart</button>
         `
     },
+    afterRender(id){
+        $('#btnAddToCart').addEventListener('click', async function(){
+            const { data } = await get(id);
+            addToCart({...data, quantity: +$("#inputQty").value})
+        })
+    }
 };
 export default DetailProductPage;
