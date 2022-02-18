adce7ab5cbb1ef9ab6be032aee30b4ef1ad11d12
Le Trong Dat <datlt2306@gmail.com>
2022-02-18 14:27:06 +0700

    refactor: Add Post - Preview image - Remove on change

--- src/pages/admin/news/add.js
+++ src/pages/admin/news/add.js
@@ -39,10 +39,18 @@ const AdminAddNewsPage = {
                         placeholder="title post"
                         id="title-post"
                         > <br />
-                    <input type="file" 
+                    <div class="grid grid-cols-2 gap-8">
+                      <div>
+                        <input type="file" 
                         class="border border-black" 
                         id="img-post"
-                        > <br />
+                        > 
+                      </div>
+                      <div>
+                        <img src="http://2.bp.blogspot.com/-MowVHfLkoZU/VhgIRyPbIoI/AAAAAAAATtI/fHk-j_MYUBs/s640/placeholder-image.jpg" id="imgPreview" />
+                      </div>
+                    </div>
+                    
                     <textarea name="" 
                             id="desc-post" 
                             cols="30" 
@@ -60,16 +68,25 @@ const AdminAddNewsPage = {
   afterRender() {
     const formAdd = document.querySelector("#form-add");
     const imgPost = document.querySelector("#img-post");
+    const imgPreview = document.querySelector('#imgPreview');
 
-    imgPost.addEventListener("change", async (e) => {
+    const CLOUDINARY_API = "https://api.cloudinary.com/v1_1/ecommercer2021/image/upload";
+    const CLOUDINARY_PRESET = "jkbdphzy"
+
+    imgPost.addEventListener('change', (e) => {
+        // console.log(URL.createObjectURL(e.target.files[0]))
+        imgPreview.src = URL.createObjectURL(e.target.files[0])
+    })
+
+    // submit form
+    formAdd.addEventListener("submit", async (e) => {
+      e.preventDefault();
       const file = e.target.files[0];
-      const CLOUDINARY_API = "https://api.cloudinary.com/v1_1/ecommercer2021/image/upload";
-
+      
       // Lấy giá trị của file upload cho sử dụng formData
       const formData = new FormData();
       formData.append("file", file);
-      formData.append("upload_preset", "jkbdphzy");
-      
+      formData.append("upload_preset", CLOUDINARY_PRESET);
         // call API
       const { data } = await axios.post(CLOUDINARY_API,formData, {
           headers: {
@@ -77,17 +94,12 @@ const AdminAddNewsPage = {
           },
         }
       );
-      // submit form
-      formAdd.addEventListener("submit", (e) => {
-        e.preventDefault();
-        add({
-          title: document.querySelector("#title-post").value,
-          img: data.url,
-          desc: document.querySelector("#desc-post").value,
-        });
-        // Sau khi thêm bài viết thành công...
+      add({
+        title: document.querySelector("#title-post").value,
+        img: data.url,
+        desc: document.querySelector("#desc-post").value,
       });
-
+      // Sau khi thêm bài viết thành công...
     });
   },
 };
