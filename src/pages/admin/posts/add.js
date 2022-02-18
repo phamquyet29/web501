1d7846c46fafe28865af66a881a51ccdc16f5b1f
Le Trong Dat <datlt2306@gmail.com>
2022-02-18 19:33:46 +0700

    refactor: Add product - Preview Image

--- src/pages/admin/posts/add.js
+++ src/pages/admin/posts/add.js
@@ -7,6 +7,7 @@ const AddPost = {
             <form id="formAddPost">
                 <input type="text" id="title-post" class="border border-black" placeholder="Title" /><br />
                 <input type="file" id="img-post" class="border border-black" placeholder="Title" /><br />
+                <img src="" id="img-preview"/>
                 <textarea name="" id="desc-post" class="border border-black" cols="30" rows="10"></textarea><br />
                 <button>Tạo bài viết</button>
             </form>
@@ -16,12 +17,20 @@ const AddPost = {
         const formAddPost = document.querySelector('#formAddPost');
         const CLOUDINARY_PRESET_KEY = "jkbdphzy";
         const CLOUDINARY_API_URL = "https://api.cloudinary.com/v1_1/ecommercer2021/image/upload";
+        const imgPost = document.querySelector('#img-post');
+        const imgPreview = document.querySelector('#img-preview')
+
         let imgLink = "";
+
+
+        imgPost.addEventListener('change', function(e){
+            imgPreview.src = URL.createObjectURL(e.target.files[0]);
+        });
 
         formAddPost.addEventListener('submit', async function(e){
             e.preventDefault();
 
-            const file = document.querySelector('#img-post').files[0];
+            const file = imgPost.files[0];
             if(file){
                 // lấy giá trị của file và gán vào object formData
                 const formData = new FormData();
