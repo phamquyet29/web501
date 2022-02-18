573b129eb09a38b2897835007c4977971b5e38f6
Le Trong Dat <datlt2306@gmail.com>
2022-02-18 19:44:48 +0700

    feat: Validate form

--- src/pages/admin/posts/add.js
+++ src/pages/admin/posts/add.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { add } from "../../../api/posts";
+import $ from 'jquery';
+import validate from 'jquery-validation';
 
 const AddPost = {
     render(){
         return /*html*/`
             <form id="formAddPost">
-                <input type="text" id="title-post" class="border border-black" placeholder="Title" /><br />
+                <input type="text" id="title-post" class="border border-black" placeholder="Title" name="title-post" /><br />
                 <input type="file" id="img-post" class="border border-black" placeholder="Title" /><br />
                 <img src="" id="img-preview"/>
                 <textarea name="" id="desc-post" class="border border-black" cols="30" rows="10"></textarea><br />
@@ -14,7 +16,7 @@ const AddPost = {
         `
     },
     afterRender(){
-        const formAddPost = document.querySelector('#formAddPost');
+        const formAddPost = $('#formAddPost');
         const CLOUDINARY_PRESET_KEY = "jkbdphzy";
         const CLOUDINARY_API_URL = "https://api.cloudinary.com/v1_1/ecommercer2021/image/upload";
         const imgPost = document.querySelector('#img-post');
@@ -26,32 +28,52 @@ const AddPost = {
         imgPost.addEventListener('change', function(e){
             imgPreview.src = URL.createObjectURL(e.target.files[0]);
         });
-
-        formAddPost.addEventListener('submit', async function(e){
-            e.preventDefault();
-
-            const file = imgPost.files[0];
-            if(file){
-                // lấy giá trị của file và gán vào object formData
-                const formData = new FormData();
-                formData.append('file', file);
-                formData.append('upload_preset', CLOUDINARY_PRESET_KEY);
-
-                // call API cloudinary để đẩy ảnh lên
-                const { data } = await axios.post(CLOUDINARY_API_URL, formData, {
-                    headers: {
-                        "Content-Type": "application/form-data"
+        formAddPost.validate({
+            rules: {
+                "title-post": {
+                    required: true,
+                    minlength: 5
+                }
+            },
+            messages: {
+                "title-post": {
+                    required: "Bắt buộc phải nhập trường này!",
+                    minlength: "Nhập ít nhất 5 ký tự"
+                }
+            },
+            submitHandler: function() {
+                async function addPost(){
+                    const file = imgPost.files[0];
+                    if(file){
+                        // lấy giá trị của file và gán vào object formData
+                        const formData = new FormData();
+                        formData.append('file', file);
+                        formData.append('upload_preset', CLOUDINARY_PRESET_KEY);
+        
+                        // call API cloudinary để đẩy ảnh lên
+                        const { data } = await axios.post(CLOUDINARY_API_URL, formData, {
+                            headers: {
+                                "Content-Type": "application/form-data"
+                            }
+                        })
+                        imgLink = data;
                     }
-                })
-                imgLink = data;
+                    // call api thêm bài viết
+                    add({
+                        title: document.querySelector('#title-post').value,
+                        img: imgLink ? imgLink : "",
+                        desc: document.querySelector('#desc-post').value,
+                    });
+                }
+                addPost();
             }
-            // call api thêm bài viết
-            add({
-                title: document.querySelector('#title-post').value,
-                img: imgLink ? imgLink : "",
-                desc: document.querySelector('#desc-post').value,
-            });
-        })
+        });
+
+        // formAddPost.addEventListener('submit', async function(e){
+        //     e.preventDefault();
+
+        
+        // })
     }
 }
 export default AddPost;
