18918f4c9801d7a46cc3b7bec15791bf17df7d35
Le Trong Dat <datlt2306@gmail.com>
2022-02-18 12:07:38 +0700

    refactor: upload image

--- src/pages/admin/news/add.js
+++ src/pages/admin/news/add.js
@@ -66,31 +66,32 @@ const AdminNewsAddPage = {
     const imgPost = document.querySelector("#img-post");
 
     imgPost.addEventListener("change", (e) => {
-      const file = e.target.files[0];
-      const formData = new FormData();
-      formData.append("file", file);
-      formData.append("upload_preset", "jkbdphzy");
-
-      axios({
-        url: "https://api.cloudinary.com/v1_1/ecommercer2021/image/upload",
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-formendcoded",
-        },
-        data: formData,
-      }).then((res) => {
-        formAdd.addEventListener("submit", (e) => {
-          e.preventDefault();
-          add({
-            title: document.querySelector("#title-post").value,
-            img: res.data.secure_url,
-            desc: document.querySelector("#desc-post").value,
-          })
-            .then((result) => console.log(result.data))
-            .catch((error) => console.log(error));
-        });
-      });
+      
     });
+    formAdd.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        const file = e.target.files[0];
+        const formData = new FormData();
+        formData.append("file", file);
+        formData.append("upload_preset", "jkbdphzy");
+
+        const {data } = await axios({
+            url: "https://api.cloudinary.com/v1_1/ecommercer2021/image/upload",
+            method: "POST",
+            headers: {
+            "Content-Type": "application/x-www-formendcoded",
+            },
+            data: formData,
+        })
+        add({
+          title: document.querySelector("#title-post").value,
+          img: data.url,
+          desc: document.querySelector("#desc-post").value,
+        })
+          .then((result) => console.log(result.data))
+          .catch((error) => console.log(error));
+      });
   },
 };
 export default AdminNewsAddPage;
