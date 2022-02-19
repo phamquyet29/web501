feed4f21873e9ff5f229fbbd1ec034ac23ba19fd
Le Trong Dat <datlt2306@gmail.com>
2022-02-19 10:50:45 +0700

    feat: add nav for banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -18,11 +18,23 @@ const Banner = {
                     <img src="https://picsum.photos/1500/400" alt="" />
                 </div>
             </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-pagination"></div>
         </div>
         `;
     },
     afterRender() {
-        const swiper = new Swiper(".swiper");
+        const swiper = new Swiper(".swiper", {
+            pagination: {
+                el: ".swiper-pagination",
+                type: "progressbar",
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+        });
     },
 };
 export default Banner;
