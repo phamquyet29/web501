693c0882ac16b0b7d288ed7998bb2096a71832e0
Le Trong Dat <datlt2306@gmail.com>
2022-05-13 16:05:04 +0700

    feat: add router

--- ontap/src/main.js
+++ ontap/src/main.js
@@ -1,10 +1,21 @@
+import Navigo from 'navigo'; // When using ES modules.
+const router = new Navigo('/');
+
 import AboutPage from './pages/about';
 import HomePage from './pages/home';
 import ProductPage from './pages/product';
 
 document.addEventListener('DOMContentLoaded', function () {
-    const app = document.getElementById('app');
-    if (app) {
-        app.innerHTML = HomePage.render();
+    function print(page) {
+        const app = document.getElementById('app');
+        if (app) {
+            app.innerHTML = page.render();
+        }
     }
+
+    router.on({
+        '/': () => print(HomePage),
+        '/about': () => print(AboutPage),
+    });
+    router.resolve();
 });
