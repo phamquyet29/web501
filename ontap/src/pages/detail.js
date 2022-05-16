ad0b54b227288b16b4ea63d8cc5041b53f1ac043
Le Trong Dat <datlt2306@gmail.com>
2022-05-16 15:41:55 +0700

    feat: update product detail

--- ontap/src/pages/detail.js
+++ ontap/src/pages/detail.js
@@ -2,8 +2,11 @@ import data from '@/data';
 
 const ProductDetail = {
     render(id) {
+        // Tìm id trong mảng data
         const product = data.find((item) => item.id === id);
+        // nếu không có thì return về string not found
         if (!product) return `<div>Not found</div>`;
+        // ngược lại nếu có trả về dữ liệu
         return `${product.name}`;
     },
 };
