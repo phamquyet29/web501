77d57f7f9a73ed1472d806bb592a3e34006671d9
Le Trong Dat <datlt2306@gmail.com>
2022-06-01 15:13:33 +0700

    update detail product

--- ontap/src/pages/detail.js
+++ ontap/src/pages/detail.js
@@ -1,12 +1,13 @@
 import banner from '@/components/banner';
 import Header from '@/components/header';
 import data from '@/data';
+import axios from 'axios';
 
 const ProductDetail = {
     async render(id) {
         // Tìm id trong mảng data
         // const product = data.find((item) => item.id === id);
-        const product = await (await fetch(`http://localhost:3001/products/${id}`)).json();
+        const { data: product } = await axios.get(`http://localhost:3001/products/${id}`);
         // nếu không có thì return về string not found
         if (!product) return `<div>Not found</div>`;
         // ngược lại nếu có trả về dữ liệu
