a5e02d7b34c726e18c8d065631a0ca122686a59a
Le Trong Dat <datlt2306@gmail.com>
2023-01-06 10:17:16 +0700

    chap02 - destructoring

--- we17307/examples/destructoring.js
+++ we17307/examples/destructoring.js
@@ -1,1 +1,17 @@
-console.log("1");
+// object = {key : value }
+const person = {
+    name: "John",
+    age: 30,
+    address: {
+        street: "Main Street",
+    },
+};
+// const myName = person.name;
+// const myAge = person.age;
+// const myAddress = person.address;
+
+function showInfo({ name, age, address: { street } }) {
+    console.log(`Name: ${name}, Age: ${age}, Street: ${street}`);
+}
+showInfo(person);
+// destructuring assignment
