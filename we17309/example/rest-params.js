48c7c36b2364d647324b26507b5a016a852beba6
Le Trong Dat <datlt2306@gmail.com>
2023-01-07 10:13:10 +0700

    buổi 3 - rest params

---
+++ we17309/example/rest-params.js
@@ -0,0 +1,8 @@
+function total(...numbers) {
+    let result = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        result += numbers[i];
+    }
+    console.log("result", result);
+}
+total(1, 2, 3, 4, 5, 6, 7);
