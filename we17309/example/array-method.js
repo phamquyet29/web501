dac40e20db6513de58bbc5d361419835b2ed89d4
Le Trong Dat <datlt2306@gmail.com>
2023-01-07 10:42:31 +0700

    Buổi 3 - Array

--- we17309/example/array-method.js
+++ we17309/example/array-method.js
@@ -1,1 +1,30 @@
 // loop
+const products = [
+    "Product A", // item
+    "Product B", // item
+    "Product C", // item
+];
+for (let i = 0; i < products.length; i++) {
+    // biết điểm dừng, dựa theo length
+    // dựa vào index để hiển thị
+    console.log("for ", products[i]);
+}
+// for in
+for (let item in products) {
+    console.log("for in ", products[item]);
+}
+// for of
+for (let item of products) {
+    console.log("for of ", item);
+}
+// forEach
+products.forEach((item, index) => console.log(`${item} ${index}`));
+
+// map -> Tạo ra 1 mảng mới và có thể thay đổi giá trị
+const newMapProducts = products.map((item, index) => {
+    return `${item} ${index + 1}`;
+});
+// filter -> Tạo ra 1 mảng, lọc ra các phần tử thỏa mãn điều kiện
+const newFilterProducts = products.filter((item) => {
+    return item != "Product C";
+});
--- we17309/example/rest-params.js
+++ we17309/example/rest-params.js
@@ -1,6 +1,11 @@
 function total(...numbers) {
     let result = 0;
     for (let i = 0; i < numbers.length; i++) {
+        // result = result + numbers[i]
+        // result = 0 + 1
+        // result = 1 + 2
+        // result = 3 + 3
+        // result = 6 + 4
         result += numbers[i];
     }
     console.log("result", result);
