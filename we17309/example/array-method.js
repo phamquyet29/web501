7279e01b96719f58e3cc03b5b57934b8a6c93d89
Le Trong Dat <datlt2306@gmail.com>
2023-01-07 11:13:38 +0700

    buổi 3 - find, map, reduce, filter

--- we17309/example/array-method.js
+++ we17309/example/array-method.js
@@ -24,7 +24,29 @@ products.forEach((item, index) => console.log(`${item} ${index}`));
 const newMapProducts = products.map((item, index) => {
     return `${item} ${index + 1}`;
 });
+
+// const app = document.querySelector("#app");
+// app.innerHTML = products.map((item) => `<div>${item}</div>`).join("");
+
 // filter -> Tạo ra 1 mảng, lọc ra các phần tử thỏa mãn điều kiện
 const newFilterProducts = products.filter((item) => {
     return item != "Product C";
 });
+// reduce
+
+const numbers = [
+    { id: 1, name: "Product A", price: 200 }, // accumulator
+    { id: 2, name: "Product B", price: 300 },
+    { id: 3, name: "Product C", price: 400 },
+];
+const total = numbers.reduce((accumulator, nextValue) => accumulator + nextValue.price, 0);
+
+// Find -> Tìm phần tử trong mảng -> trả về 1 phần tử thỏa mãn điều kiện
+const products2 = [
+    { id: 1, name: "Product A", price: 200 }, // item
+    { id: 2, name: "Product B", price: 300 }, // item
+    { id: 3, name: "Product C", price: 400 }, // item
+];
+
+const foundProduct = products2.find((item) => item.id == 2);
+console.log(foundProduct);
