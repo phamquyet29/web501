a452d42f9ce1505e45f6cc024a85f3beff9edbfe
Le Trong Dat <datlt2306@gmail.com>
2023-01-07 13:25:34 +0700

    buổi 3 - map method

--- we17302/examples/array.js
+++ we17302/examples/array.js
@@ -11,7 +11,37 @@ for (let i in products) {
 for (let item of products) {
     // console.log(item);
 }
+
 // forEach
 products.forEach(function (item, i) {
     console.log(`${item} có index là : ${i}`);
 });
+
+const productList = [
+    { id: 1, name: "Product A", price: 200 }, // item
+    { id: 2, name: "Product B", price: 300 }, // item
+    { id: 3, name: "Product C", price: 400 }, // item
+];
+const productListElement = document.querySelector("#productList");
+// Ví dụ forEach
+
+// const showProducts = () => {
+//     let result = "";
+//     productList.forEach((item) => {
+//         result += `<div>${item.name}</div>`;
+//     });
+//     productListElement.innerHTML = result;
+// };
+// showProducts();
+
+// map - method
+console.log("productList", productList);
+const newProductList = productList.map((item) => {
+    return `${item.name} + ahihi`;
+});
+console.log(newProductList);
+
+const showProducts = () => {
+    productListElement.innerHTML = productList.map((item) => `<div>${item.name}</div>`).join("");
+};
+showProducts();
