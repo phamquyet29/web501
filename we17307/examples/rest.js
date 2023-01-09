835ec14442bb28c8ed3b0132a98893bda44f81c3
Le Trong Dat <datlt2306@gmail.com>
2023-01-09 10:05:04 +0700

    rest params

---
+++ we17307/examples/rest.js
@@ -0,0 +1,14 @@
+// rest parameter
+
+const add = (x, y, ...numbers) => {
+    console.log("x", x); // 1
+    console.log("y", y); // 2
+    console.log(numbers); // [3,4,5]
+};
+add(1, 2, 3, 4, 5);
+const showObject = ({ a, b, ...other }) => {
+    console.log("a", a); // a
+    console.log("b", b); // b
+    console.log("other", other); // { c : 3, d: 4}
+};
+showObject({ a: 1, b: 2, c: 3, d: 4 });
