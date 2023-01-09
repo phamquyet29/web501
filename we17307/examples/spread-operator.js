43821598f29abe8be0bf92b3aefc54519a88103c
Le Trong Dat <datlt2306@gmail.com>
2023-01-09 09:59:06 +0700

    shallow copy & deep copy

--- we17307/examples/spread-operator.js
+++ we17307/examples/spread-operator.js
@@ -2,22 +2,34 @@ const person = {
     name: "John",
     age: 30,
     isMarried: true,
+    child: {
+        name: "Kien",
+    },
 };
-const person2 = {
-    ...person,
-    address: "Main Street",
-};
-console.log(person2);
 
+// Shallow copy -> copy nông
+// ====== Sử dụng spread operator để copy object
+// const person2 = {
+//     ...person,
+//     address: "Main Street",
+// };
+
+// ====== Sử dụng Object.assign để copy object
+// const person2 = Object.assign({ address: "Main street" }, person);
+// console.log(person2);
+
+// deep copy -> copy sâu
+// const person2 = JSON.parse(JSON.stringify(person));
+const person2 = structuredClone(person);
+
+// Sử dụng deep copy để thay đổi giá trị B
+person2.child.name = "Dat";
+console.log("person 1", person);
+console.log("person 2", person2);
+
+// ====== Sử dụng spread-operator copy phần tử mảng
 const products = ["Product A", "Product B"];
 // products.push("Product C");
 const newProducts = [...products, "Product C"];
 console.log("products", products);
 console.log("newProducts", newProducts);
-
-// rest parameter
-
-const add = (...numbers) => {
-    console.log("numbers", numbers);
-};
-add(1, 2, 3, 4, 5, 6);
