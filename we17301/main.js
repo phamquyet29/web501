1df3dd5983b69f5b4871508e5ac5746adf7f77d6
Le Trong Dat <datlt2306@gmail.com>
2023-01-09 13:28:14 +0700

    Buổi 4 - update router

--- we17301/main.js
+++ we17301/main.js
@@ -1,13 +1,18 @@
 import HomePage from "./pages/home";
 import ContactPage from "./pages/contact";
 import AboutPage from "./pages/about";
+import PostPage from "./pages/post-detail";
+import PostsPage from "./pages/posts";
 import { render, router } from "./lib";
 
 const app = document.querySelector("#app");
 
 router.on("/", () => render(HomePage, app));
 router.on("/about", () => render(AboutPage, app));
-
+router.on("/contact", () => render(ContactPage, app));
+router.on("/posts", () => render(PostsPage, app));
+router.on("/post/:id", () => render(PostPage, app));
+//npm i navigo --save
 router.resolve();
 
 // render(HomePage, app);
