b85b163ee36e64497af7d5422ad886b1601edd70
Le Trong Dat <datlt2306@gmail.com>
2023-01-10 13:48:47 +0700

    buổi 4 - create page

--- we17302/main.js
+++ we17302/main.js
@@ -1,11 +1,22 @@
-import HomePage from "./pages/home";
-import AboutPage from "./pages/about";
-import NotFoundPage from "./pages/not-found";
 import { render, router } from "./lib";
+import AboutPage from "./pages/about";
+import ContactPage from "./pages/contact";
+import HomePage from "./pages/home";
+import NotFoundPage from "./pages/not-found";
+import PostDetailPage from "./pages/post-detail";
+import PostsPage from "./pages/posts";
+import ProjectDetailPage from "./pages/project-detail";
+import ProjectsPage from "./pages/projects";
 
 const app = document.querySelector("#app");
+
 router.on("/", () => render(HomePage, app));
 router.on("/about", () => render(AboutPage, app));
+router.on("/contact", () => render(ContactPage, app));
+router.on("/projects", () => render(ProjectsPage, app));
+router.on("/project/:projectId", () => render(ProjectDetailPage, app));
+router.on("/post/:postId", () => render(PostDetailPage, app));
+router.on("/posts", () => render(PostsPage, app));
 router.notFound(() => render(NotFoundPage, app));
 
 router.resolve();
--- we17302/pages/projects.js
+++ we17302/pages/projects.js
@@ -1,5 +1,17 @@
 const ProjectsPage = () => {
-    return `<h1>Projects Page</h1>`;
+    const projectList = [
+        { id: 1, name: "Dự án 1" },
+        { id: 2, name: "Dự án 2" },
+    ];
+
+    return `<h1>Projects Page</h1>
+        ${projectList
+            .map((item) => {
+                return `<div><a href="/project/${item.id}">${item.name}</a></div>`;
+            })
+            .join("")}
+    
+    `;
 };
 
 export default ProjectsPage;
