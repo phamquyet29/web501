43914d72a4ea68114f08dc0d5d2c86664807040b
Le Trong Dat <datlt2306@gmail.com>
2023-01-10 09:50:18 +0700

    Buổi 4 - Ôn tập shallow clone & deep clone

--- we17309/example/rest-params.js
+++ we17309/example/rest-params.js
@@ -1,3 +1,10 @@
+function show({ a, b, ...other }) {
+    console.log(a); // 1
+    console.log(b); // 2
+    console.log(other); // { c: 3, d: 4}
+}
+show({ a: 1, b: 2, c: 3, d: 4 });
+
 function total(...numbers) {
     let result = 0;
     for (let i = 0; i < numbers.length; i++) {
--- we17309/example/spread-operator.js
+++ we17309/example/spread-operator.js
@@ -1,18 +1,29 @@
-// spread-operator -> copy
 const person = {
     name: "Dat",
     age: 20,
+    child: {
+        name: "Tai",
+    },
 };
 
-const person2 = {
-    ...person,
-    address: "Thai Binh",
-};
+// // spread-operator -> copy
+// const person2 = {
+//     ...person,
+//     address: "Thai Binh",
+// };
 
-console.log(person2);
+// // Object assign
+// const person2 = Object.assign({ address: "Thai Binh" }, person);
 
-const products = [1, 2, 3, 4];
+// const person2 = JSON.parse(JSON.stringify(person));
+const person2 = structuredClone(person);
+person2.child.name = "Kien";
+
+console.log("person 1", person);
+console.log("person 2", person2);
+
+// const products = [1, 2, 3, 4];
 // products.push(5);
-console.log("products", products);
-const newProducts = [-1, ...products, 5];
-console.log("newProducts", newProducts); // [1,2,3,4,5];
+// console.log("products", products);
+// const newProducts = [-1, ...products, 5];
+// console.log("newProducts", newProducts); // [1,2,3,4,5];
