3d21dfb2db3133d8df84da83e0d19b9d4a21218f
Le Trong Dat <datlt2306@gmail.com>
2023-01-11 11:07:24 +0700

    Buổi 4 - show projects

--- we17307/pages/projects.js
+++ we17307/pages/projects.js
@@ -6,11 +6,11 @@ const ProjectsPage = () => {
     ${Header()}
     <h1>Projects Page</h1>
     <div class="projects">
-        ${projects.map((project) => {
-            return `<div class="project-item">${project.name}</div>`;
-        })}
-        <div>item 1</div>
-        <div>item 2</div>
+        ${projects
+            .map((project) => {
+                return `<div class="project-item">${project.name}</div>`;
+            })
+            .join("")}
     </div>
     
     `;
