3ad8455799028a46b563e7ce3d31bfddaccc4352
Le Trong Dat <datlt2306@gmail.com>
2023-01-11 10:47:36 +0700

    buổi 4 - array

--- we17307/pages/projects.js
+++ we17307/pages/projects.js
@@ -1,8 +1,18 @@
 import Header from "../components/Header";
+import { projects } from "../data";
 
 const ProjectsPage = () => {
     return `
     ${Header()}
-    <h1>Projects Page</h1>`;
+    <h1>Projects Page</h1>
+    <div class="projects">
+        ${projects.map((project) => {
+            return `<div class="project-item">${project.name}</div>`;
+        })}
+        <div>item 1</div>
+        <div>item 2</div>
+    </div>
+    
+    `;
 };
 export default ProjectsPage;
