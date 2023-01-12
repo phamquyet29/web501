e9ab789561f5c5fb8d5f4938b89b78328e4adcc5
Le Trong Dat <datlt2306@gmail.com>
2023-01-12 13:22:52 +0700

    buổi 5: Show & check project detail

--- we17302/src/pages/project-detail.js
+++ we17302/src/pages/project-detail.js
@@ -4,10 +4,25 @@ const ProjectDetailPage = ({ data: { projectId } }) => {
     // find
     const currentProject = projectsFake.find((project) => project.id == projectId);
     if (!currentProject) return `Loading...`;
-    // project/:id
-    // new URL().searchParams.get('id')
     return `<h1>Project Detail Page</h1>
         ${currentProject.name}
+        <hr />
+        <h2>Teams</h2>
+        ${
+            currentProject.teams
+                ? `
+                    <ul>
+                        ${currentProject.teams
+                            .map((member) => {
+                                return `<li>${member.name}</li>`;
+                            })
+                            .join("")}
+                        
+                    <ul>
+                    `
+                : "Không có"
+        }
+        
     `;
 };
 
