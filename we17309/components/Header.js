74e22cab173b797fe88a5f3ef2c704ec1bf5c6af
Le Trong Dat <datlt2306@gmail.com>
2023-01-12 09:54:10 +0700

    Buổi 5 - create component header

---
+++ we17309/components/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+    return `
+        <header>
+            <nav>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="">Projects</a>
+                <a href="">Post</a>
+                <a href="/contact">Contact</a>
+            </nav>
+        </header>
+    
+    `;
+};
+export default Header;
--- we17309/pages/home.js
+++ we17309/pages/home.js
@@ -1,4 +1,9 @@
+import Header from "../components/Header";
+
 const HomePage = () => {
-    return `<h1>Home Page</h1>`;
+    return `
+    ${Header()}
+    
+    <h1>Home Page</h1>`;
 };
 export default HomePage;
