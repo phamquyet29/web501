9fbc6c1553abc1189fbdcd5617e67cd198a4d93f
Le Trong Dat <datlt2306@gmail.com>
2023-01-12 13:25:30 +0700

    Buổi 5 - update detail

--- we17302/src/pages/project-detail.js
+++ we17302/src/pages/project-detail.js
@@ -4,23 +4,19 @@ const ProjectDetailPage = ({ data: { projectId } }) => {
     // find
     const currentProject = projectsFake.find((project) => project.id == projectId);
     if (!currentProject) return `Loading...`;
-    return `<h1>Project Detail Page</h1>
+    return /*html*/ `<h1>Project Detail Page</h1>
         ${currentProject.name}
         <hr />
-        <h2>Teams</h2>
+        
         ${
             currentProject.teams
                 ? `
+                    <h2>Teams</h2>
                     <ul>
-                        ${currentProject.teams
-                            .map((member) => {
-                                return `<li>${member.name}</li>`;
-                            })
-                            .join("")}
-                        
+                        ${currentProject.teams.map((member) => `<li>${member.name}</li>`).join("")}
                     <ul>
                     `
-                : "Không có"
+                : ""
         }
         
     `;
