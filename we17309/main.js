e1b1a6e19382a1334401b46b9b7ea3e8b34a93cd
Le Trong Dat <datlt2306@gmail.com>
2023-01-14 10:59:32 +0700

    Buổi 6 - Add bootstrap và config sử dụng cả 2 thư viện css

--- we17309/main.js
+++ we17309/main.js
@@ -1,3 +1,7 @@
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 import { render, router } from "./src/lib";
 import AboutPage from "./src/pages/about";
 import ContactPage from "./src/pages/contact";
