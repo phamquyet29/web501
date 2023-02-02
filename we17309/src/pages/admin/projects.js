82db50fb6ee625fde1e796ff3d8c92f7dbcb55b5
Le Trong Dat <datlt2306@gmail.com>
2023-02-02 10:29:33 +0700

    Buổi 8: feat: list & remove project

--- we17309/src/pages/admin/projects.js
+++ we17309/src/pages/admin/projects.js
@@ -1,6 +1,31 @@
 import { projects } from "../../data";
-
+import { useEffect, useState } from "../../lib";
 const ProjectsPage = () => {
+    /**
+     *
+     * Bước 1: Khởi tạo state
+     * Bước 2: render HTML
+     * Bước 3: Chạy vào hàm useEffect
+     *    - Lấy ra các nút remove
+     *    - Thêm sự kiện click cho các nút remove
+     */
+    const [data, setData] = useState(projects); //
+
+    useEffect(() => {
+        // 3
+        const btns = document.querySelectorAll(".btn-remove");
+        for (let btn of btns) {
+            btn.addEventListener("click", function () {
+                const id = this.dataset.id;
+                removeItem(id);
+            });
+        }
+    });
+
+    const removeItem = (id) => {
+        setData(data.filter((project) => project.id != id));
+    };
+    // 2
     return `<div>
         <h1>Quản lý dự án</h1>
         <table class="table table-bordered">
@@ -12,14 +37,16 @@ const ProjectsPage = () => {
                 </tr>
             </thead>
         <tbody>
-            ${projects
+            ${data
                 .map(
                     (project, index) => `
                 <tr>
                     <td>${index + 1}</td>
                     <td>${project.name}</td>
                     <td>
-                        <button class="btn btn-danger">Remove</button>
+                        <button data-id="${
+                            project.id
+                        }" class="btn btn-remove btn-danger">Remove</button>
                     </td>
                 </tr>
             `
