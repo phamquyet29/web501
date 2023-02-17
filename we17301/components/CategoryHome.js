3e5ecf1bbfdf2e16c16ccdb122184c23e465848e
Le Trong Dat <datlt2306@gmail.com>
2023-02-17 12:40:11 +0700

    Example: show project in category

---
+++ we17301/components/CategoryHome.js
@@ -0,0 +1,32 @@
+import { useEffect } from "../lib";
+
+const CategoryHome = ({ categories, onClick }) => {
+    useEffect(() => {
+        const btns = document.querySelectorAll(".btn");
+        for (let btn of btns) {
+            btn.addEventListener("click", function () {
+                const id = btn.dataset.id;
+                onClick(id);
+            });
+        }
+    });
+    return `<div>
+        ${categories
+            .map(
+                (category) => `
+            <button class="btn" data-id="${category.id}">${category.name}</button>
+        `
+            )
+            .join("")}
+    </div>`;
+};
+
+export default CategoryHome;
+
+function show() {}
+
+function calcular(a, b, callback) {
+    callback();
+}
+
+calcular(5, 5, show);
--- we17301/pages/home.js
+++ we17301/pages/home.js
@@ -1,8 +1,29 @@
 import Header from "../components/Header";
-
+import CategoryHome from "../components/CategoryHome";
+import ProjectsHome from "../components/ProjectsHome";
+import { useEffect, useState } from "../lib";
 const HomePage = () => {
+    const [categories, setCategories] = useState([]);
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3001/categoryProjects")
+            .then((response) => response.json())
+            .then((data) => setCategories(data));
+    }, []);
+
+    const onHandleClick = (id) => {
+        fetch(`http://localhost:3001/categoryProjects/${id}?_embed=projects`)
+            .then((response) => response.json())
+            .then((data) => setProjects(data.projects));
+    };
     return `
         ${Header()}
+
+        <hr />
+
+        ${CategoryHome({ categories, onClick: onHandleClick })}
+        ${ProjectsHome({ projects })}
     `;
 };
 export default HomePage;
