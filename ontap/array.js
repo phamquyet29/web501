6687018ab213fed97a2838d98ada842dd15597a7
Le Trong Dat <datlt2306@gmail.com>
2023-05-10 11:09:17 +0700

    Update buổi 2

---
+++ ontap/array.js
@@ -0,0 +1,24 @@
+const myArr = ["Dat", "Kien", "Son"];
+const app = document.getElementById("app");
+
+let content = "";
+
+// for
+// for (let i = 0; i < myArr.length; i++) {
+//     content += `<li>${myArr[i]}</li>`;
+// }
+
+// for...in
+// for (let i in myArr) {
+//     content += `<li>${myArr[i]}</li>`;
+// }
+
+// for..of
+// for (let name of myArr) {
+//     content += `<li>${name}</li>`;
+// }
+
+// forEach
+myArr.forEach((name, index) => (content += `<li>${name} + ${index}</li>`));
+
+app.innerHTML = `<ul> ${content}</ul>`;
