2d3ca30747c44d92ef6570410e2af41b944e7291
Le Trong Dat <datlt2306@gmail.com>
2023-05-15 10:46:52 +0700

    Buổi 4

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,25 @@
+import Banner from "../components/Banner";
 import Header from "../components/Header";
+import { posts } from "../dataFake";
 
 const HomePage = () => {
     return `
         <div class="container max-w-4xl mx-auto px-4">
             ${Header()}
+            ${Banner()}
+            <div>
+                <h2>Bài viết</h2>
+                <div class="grid grid-cols-3 gap-8">
+                    ${posts
+                        .map(function (post) {
+                            return `<div>
+                            <h3>${post.title}</h3>
+                            <p>${post.content}</p>
+                        </div>`;
+                        })
+                        .join("")}
+                </div>
+            </div>
         </div>
     `;
 };
