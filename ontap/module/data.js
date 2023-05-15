8ccb40b3f32f64b6bf27a270d7f50a0edbc3509e
Le Trong Dat <datlt2306@gmail.com>
2023-05-15 09:57:16 +0700

    Buổi 4

---
+++ ontap/module/data.js
@@ -0,0 +1,4 @@
+const a = 10;
+const b = 20;
+
+export { a, b };
---
+++ ontap/module/showdata.js
@@ -0,0 +1,7 @@
+import { a, b } from "./data";
+
+function sum(valueA, valueB) {
+    console.log(valueA + valueB);
+}
+
+sum(a, b);
