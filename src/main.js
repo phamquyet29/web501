3ee8278c703baaad6b1d915b33bfb0213e459d31
Le Trong Dat <datlt2306@gmail.com>
2023-05-15 09:42:25 +0700

    Buổi 4

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+
+const app = document.querySelector("#app");
+
+app.innerHTML = HomePage();
---
+++ src/pages/HomePage.js
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return `
+        <div>
+            <h1 class="bg-red-500">Home Page</h1>
+        </div>
+    `;
+};
+
+export default HomePage;
