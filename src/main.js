de1bca750e9421f1be86ecfa35cb20fd87317f19
Le Trong Dat <datlt2306@gmail.com>
2023-05-17 10:46:22 +0700

    Buổi 5

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
+import Navigo from "navigo"; // When using ES modules.
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
 
-const app = document.querySelector("#app");
+const router = new Navigo("/");
 
-app.innerHTML = HomePage();
+const render = (content) => {
+    const app = document.querySelector("#app");
+    app.innerHTML = content();
+};
+
+router.on("/", function () {
+    render(HomePage);
+});
+router.on("/about", function () {
+    render(AboutPage);
+});
+
+router.resolve();
