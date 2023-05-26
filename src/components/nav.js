9d39e95565d51a0de7828623520743413738170b
quyet <phamquyet3377@gmail.com>
2023-05-26 13:49:54 +0700

    test

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,8 +5,8 @@ export const nav = () =>{
         <label class="changeTheme">
           <input type="checkbox" name="changeTheme" />
         </label>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#skill">Skill</a></li>
+        <li> <a href="#home">Home</a></li>
+        <li> <a href="#skill">Skill</a></li>
         <li><a href="#project">Project</a></li>
         <li><a href="#contact">Contact</a></li>
         
